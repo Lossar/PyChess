d38d1dd3057f4ffd0feeb892ae95f9468b1dbde6
Lossar <larsivarramberg@yahoo.no>
2022-02-03 15:00:27 +0100

    Added ability to move pieces

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 # This is a sample Python script.
 from board import ChessBoard
 
+
+def move_and_update(init_x, init_y, target_x, target_y):
+    chessboard.attempt_move(init_x, init_y, target_x, target_y)
+    chessboard.print_board()
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     chessboard = ChessBoard()
-    chessboard.print_board()
+    move_and_update(1, 1, 1, 2)
+    move_and_update(1, 1, 2, 1)
+    move_and_update(1, 1, 1, 1)
+    move_and_update(2, 1, 4, 1)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
