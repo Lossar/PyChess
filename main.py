ad96dc47cbc1a6a3ac645d69f6f2d0083958bb69
lossar <larsivarramberg@yahoo.no>
2022-05-25 20:59:51 +0200

    Add basic game loop

--- main.py
+++ main.py
@@ -10,14 +10,18 @@ def move_and_update(init_x, init_y, target_x, target_y):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     chessboard = ChessBoard()
-    move_and_update(1, 1, 1, 2)
-    move_and_update(1, 1, 2, 1)
-    move_and_update(1, 1, 1, 1)
-    move_and_update(2, 1, 4, 1)
-
     running = True
     white_turn = True
 
     while running:
-        print("Player ", "White" if white_turn else "Black", " Please pick a piece")
+        print("Player", "{},".format("White" if white_turn else "Black"), "please pick a piece")
         piece = input()
+        validMove = True #TODO implement actual check of valid moves
+
+        if validMove:
+            #TODO Do move
+            print("Move happens here")
+            white_turn = not white_turn
+        else:
+            print("Move was invalid here")
+            #TODO retry move
