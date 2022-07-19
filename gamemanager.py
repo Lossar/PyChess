8cadf24b6c39d1fa626267976093968f6b9d7cf6
lossar <larsivarramberg@yahoo.no>
2022-07-19 23:20:10 +0200

    Add informative feedback regarding x and y positions

--- gamemanager.py
+++ gamemanager.py
@@ -5,12 +5,12 @@ class GameManager:
     def __init__(self, ui, chessboard):
         self.chessboard_ = chessboard
         self.ui = ui
-        self.ui.on_board_update(self.chessboard_)
 
     def run(self):
         x, y, target_y, target_x = None, None, None, None
 
         while x is None and y is None:
+            self.ui.on_board_update(self.chessboard_)
             self.ui.prompt_pick_piece(self.white_turn)
             x, y = self.ui.receive_input()
 
@@ -20,6 +20,7 @@ class GameManager:
             return
 
         while target_x is None and target_y is None:
+            self.ui.on_board_update(self.chessboard_)
             self.ui.prompt_pick_target(self.white_turn)
             target_x, target_y = self.ui.receive_input()
 
@@ -27,7 +28,6 @@ class GameManager:
             self.white_turn = not self.white_turn
         else:
             self.ui.inform_invalid_move(x, y, target_x, target_y)
-        self.ui.on_board_update(self.chessboard_)
 
     def move_and_update(self, init_x, init_y, target_x, target_y):
         self.chessboard_.attempt_move(init_x, init_y, target_x, target_y)
--- textui.py
+++ textui.py
@@ -1,4 +1,4 @@
-from CharToNum import char_to_num
+from CharToNum import char_to_num, num_to_char
 
 class TextUi:
     x_y_limit = 7
@@ -24,11 +24,12 @@ class TextUi:
     def prompt_pick_target(self, white_turn):
         print("Player", "{},".format("White" if white_turn else "Black"), "please pick a target")
 
-    def inform_invalid_move(self):
-        print("Move was invalid! Piece could not reach target")
+    def inform_invalid_move(self, x, y, target_x, target_y):
+        print("Move was invalid! Piece at {}{} could not reach target {}{}"
+              .format(num_to_char[x], y, num_to_char[target_x], target_y))
 
     def receive_input(self):
-        input_coord = input()
+        input_coord = input().lower()
 
         return [char_to_num[input_coord[0]], int(input_coord[1]) - 1] if self.is_valid_input(input_coord) else [None, None]
 
@@ -37,10 +38,14 @@ class TextUi:
             print("Input is not correct length! correct length is 2")
             return False
 
-        x, y = char_to_num[input_coord[0]], int(input_coord[1])
+        x, y = input_coord[0], int(input_coord[1])
+
         if self.is_x_valid(x) is False:
-            print("X is invalid!")
+            print("X of value {} is invalid! Only letters a through h are valid".format(x))
             return False
+        else:
+            x = char_to_num[x]
+
         if self.is_y_valid(y) is False:
             print("Y is invalid!")
             return False
@@ -48,10 +53,10 @@ class TextUi:
         return True
 
     def is_y_valid(self, y):
-        return y <= self.x_y_limit
+        return 1 <= y <= self.x_y_limit
 
     def is_x_valid(self, x):
-        return 0 <= x <= 7
+        return 97 <= ord(x) <= 103
 
     def is_input_valid_lenght(self, input_coord):
         return len(input_coord) == 2
